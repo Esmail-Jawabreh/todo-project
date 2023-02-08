776b672591fb2e69b4a7615149d247a76ee6df26
Esmail Jawabreh <esmailjawabreh@gmail.com>
2023-02-08 18:14:36 +0000

    JS2 updates

--- app.js
+++ app.js
@@ -34,3 +34,41 @@ if (confirm("confirm if you wants to skip the welcoming message.") == true) {
     alert("Welcome " + Name)
 }
 
+alert("Please answer the 3 following questions " + Name + ".");
+
+let Pet_Question = prompt("Do you have a pet ? yes/no");
+let Amman_Question = prompt("Do you live in Amman ? yes/no");
+let winter_Question = prompt("Do you like Winter ? yes/no");
+
+
+const Answer_Array = [];
+
+function Array_Fun(Answer_Array){
+    if (Pet_Question == "yes" || Pet_Question == "no"){
+        Answer_Array.push(Pet_Question);
+    }
+    else{
+        Answer_Array.push("invalid");
+    }
+
+
+    if (Amman_Question == "yes" || Amman_Question == "no"){
+        Answer_Array.push(Amman_Question);
+    }
+    else{
+        Answer_Array.push("invalid");
+    }
+
+    
+    if (winter_Question == "yes" || winter_Question == "no"){
+        Answer_Array.push(winter_Question);
+    }
+    else{
+        Answer_Array.push("invalid");
+    }
+
+    return Answer_Array;
+}
+
+console.log(Array_Fun(Answer_Array));
+
